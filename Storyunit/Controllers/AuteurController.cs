8bfa6f79a9f24a47aa190e3c1c5719cd8b51d363
Yari Jaspers <djaspers01@student.rocvantwente.nl>
2019-06-17 11:16:39 +0200

    auteurController aangemaakt

---
+++ Storyunit/Controllers/AuteurController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Storyunit.Controllers
+{
+    public class AuteurController : Controller
+    {
+        // GET: Auteur
+        public ActionResult Index(string auteur)
+        {
+            var data = Models.spAllboekenAuteur_Result(auteur);
+            return View(data.Tolist());
+        }
+    }
+}
