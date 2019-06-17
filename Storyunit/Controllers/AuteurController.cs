ed225706dcdf4d77b94bd4a85100d8527018e21f
Yari Jaspers <djaspers01@student.rocvantwente.nl>
2019-06-17 11:38:10 +0200

    auteur controller test

--- Storyunit/Controllers/AuteurController.cs
+++ Storyunit/Controllers/AuteurController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Storyunit.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,12 @@ namespace Storyunit.Controllers
 {
     public class AuteurController : Controller
     {
+        private dbStoryunitEntities db = new dbStoryunitEntities();
+
         // GET: Auteur
         public ActionResult Index(string auteur)
         {
-            var data = Models.spAllboekenAuteur_Result(auteur);
+            var data = Models.spAllboekenAuteur(auteur);
             return View(data.Tolist());
         }
     }
